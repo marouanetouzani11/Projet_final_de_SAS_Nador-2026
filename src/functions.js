131d80fd7cc572940168d810362280bf6f9d5855
marouanetouzani11 <marouane1games@gmail.com>
2026-09-16 21:55:35 +0100

    creation de menu

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,16 @@
 const {apprenants} = require("./data")
 
 console.log(apprenants)
+function afichageMenu(){
+	console.log("=======================================")
+	console.log("=        SAS PROGRESS CONSOLE         =")
+	console.log("=======================================")
+	console.log("")
+	console.log("\n")
+	console.log("\n")
+	console.log("\n")
+	console.log("\n")
+	console.log("\n")
+
+
+}
