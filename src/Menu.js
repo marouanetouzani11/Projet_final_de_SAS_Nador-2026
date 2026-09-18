6a40206988ed8a14d14861bd611c2f198e46c04b
marouanetouzani11 <marouane1games@gmail.com>
2026-09-18 20:30:13 +0100

    hundling the the 5 opteion in switch case adding trim to the challenge to remove spaces in the begginig and ending of string. and to tolowerCase() to make chars lower

--- src/Menu.js
+++ src/Menu.js
@@ -7,6 +7,7 @@ const {
   ajouter_Apprenant,
   consulterApprenantParId,
   Rechercher_par_nom,
+  Ajouter_modifier_résultat
   
 } = require("./functions");
 
@@ -43,7 +44,13 @@ do {
       break;
 
     case 5:
-      dicoration("5- Ajouter ou modifier le résultat d'une journée: Coming Soon");
+      dicoration("5- Ajouter ou modifier le résultat d'une journée.");
+	  let Id = Number(prompt("Entrez un identifiant numérique d'apprenants: "))
+	  let jour =  Number(prompt("Entrez le jour: "))
+	  let totalExercices = Number(prompt("Entrez un total d'exercices d'apprenants: "))
+	  let exercicesTermines = Number(prompt("Entrez exercices Termines: "))
+	  let challengeTermine = prompt("Si le challenge est relevé, écrivez oui: ").trim().toLocaleLowerCase()
+	  console.log(Ajouter_modifier_résultat(Id, jour, totalExercices, exercicesTermines, challengeTermine))
       break;
 
     case 6:
